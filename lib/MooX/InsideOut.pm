ebc65c639045ec5a7ae520871a488c38420293c5
Graham Knop <haarg@haarg.org>
2013-02-02 17:57:57 -0500

    add docs and version

--- lib/MooX/InsideOut.pm
+++ lib/MooX/InsideOut.pm
@@ -1,6 +1,9 @@
 package MooX::InsideOut;
 use strictures 1;
 
+our $VERSION = 0.001000;
+$VERSION = eval $VERSION;
+
 use Moo ();
 use Moo::Role ();
 
@@ -21,3 +24,45 @@ sub import {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+MooX::InsideOut - Inside out objects for Moo
+
+=head1 SYNOPSIS
+
+    package MyClass;
+    use Moo;
+    use MooX::InsideOut;
+
+=head1 DESCRIPTION
+
+MooX::InsideOut causes all accessors generated to be stored outside
+the object itself.  This is useful for extending non-Moo classes
+whose internals you don't want to change, or which aren't hash
+based.
+
+Inside out objects are not recommended for general use aside from
+where they are specifically required.
+
+=head1 AUTHOR
+
+haarg - Graham Knop (cpan:HAARG) <haarg@haarg.org>
+
+=head2 CONTRIBUTORS
+
+None so far.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2013 the MooX::InsideOut L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
+
+=cut
